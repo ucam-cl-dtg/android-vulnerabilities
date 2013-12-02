45deec1a8e2a70731ba99f057290a254e52e8493
Daniel Thomas <drt24@cam.ac.uk>
2013-12-02 18:16:25 +0000

    Start work on producing markdown output for each vulnerability

--- macros.py
+++ macros.py
@@ -5,9 +5,32 @@ import os
 import dateutil.parser
 from collections import defaultdict
 
+class DateRef:
+	def __init__(self, field, vuln):
+		if None == field:
+			raise ValueError("Nothing to extract date from: None")
+		if isinstance(field, dict):
+			self.datestring = field['date']
+			self.ref = field['ref']
+		elif isinstance(field, list):
+			self.datestring = field[0]
+			if len(field) == 2:
+				self.ref = field[1]
+			else:
+				self.ref = None
+		else:
+			raise ValueError("Unexpected type of field %s: %s" % (year_field, field))
+		self.date = dateutil.parser.parse(self.datestring)
+		self.vuln = vuln
+	def __str__(self):
+		string = self.datestring
+		if self.ref is not None:
+			string += self.vuln._str_reference(self.ref)
+		return string
+	
 # Class definition for a vulnerability
 class Vulnerability:
-	year_fields = ['Discovered_on','Submitted_on','Reported_on','Fixed_on','Fix_released_on']
+	year_fields = ['Discovered_on','Reported_on','Fixed_on','Fix_released_on']#'Submitted_on',
 	def __init__(self,jsn):
 		self.jsn = jsn
 		self.name = jsn['name']
@@ -15,27 +38,59 @@ class Vulnerability:
 		yrs = []
 		for year_field in self.year_fields:
 			field = self.jsn[year_field]
-			if None == field:
+			try:
+				dateref = DateRef(field, self)
+			except ValueError as e:
+				print e #TODO stderr
 				continue
-			if isinstance(field, dict):
-				datestring = field['date']
-			elif isinstance(field, list):
-				datestring = field[0]
-			else:
-				print("Unexpected type of field %s: %s" % (year_field, field))
-				continue
-			date = dateutil.parser.parse(datestring)
-			yrs.append(str(date.year))
-		return yrs
+			yrs.append(str(dateref.date.year))
+		return set(yrs)
 	def versions(self):
 		return []#TODO
 	def manufacturers(self):
-		return []#TODO
+		return self.jsn['Affected_manufacturers']
 	def submitters(self):
-		return []#TODO
+		submitterslist = self.jsn['Submitted_by']
+		return submitterslist
+	def _get_reference_url(self,reference):
+		return self.jsn['references'][reference]['url']
+	def _str_reference(self,reference):
+		return "[{reference}]({url})".format(reference=reference,url=self._get_reference_url(reference))
+	def _print_ref_list(self,reflist,separator=","):
+		answer = []
+		for itemref in reflist:
+			if isinstance(itemref, list):
+				itemstr = itemref[0]
+				if len(itemref) == 2:
+					itemstr += " " + self._str_reference(itemref[1])
+			else:
+				print("Unknown type of itemref:", type(itemref))
+			#if isinstance(itemref, dict):
+				#TODO we don't use this yet
+			answer.append(itemstr)
+		return separator.join(answer)
+
+	def __str__(self):
+		return """### {name}
+
+* CVE numbers: {cve}
+* Responsibly disclosed?: {responsibly}
+* Details: {details}
+* Discovered by: {discovered_by} on: {discovered_on}
+* 
+""".format(name=self.name,
+		cve=self._print_ref_list(self.jsn['CVE']),
+		responsibly=self.jsn['Responsibly_disclosed'],
+		details=self._print_ref_list(self.jsn['Details'], separator="\n"),
+		discovered_by=self._print_ref_list(self.jsn['Discovered_by']),
+		discovered_on=DateRef(self.jsn['Discovered_on'], self)
+		)
+	def __repr__(self):
+		return self.__str__()
 
 vulnerabilities = []
-# Key to list of vulnerability dicts TODO these need sorting
+# Key to list of vulnerability dicts
+# TODO these need sorting
 by_year = defaultdict(list)
 by_version = defaultdict(list)
 by_manufacturer = defaultdict(list)
