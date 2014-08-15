fb7045c74255956fc58abe4cd673fe06ca10fcd6
Daniel Thomas <drt24@cam.ac.uk>
2014-08-15 13:39:57 +0100

    Make version_finder more generic so that it can cope with other projects

--- version-finder.sh
+++ version-finder.sh
@@ -3,14 +3,17 @@
 # Run this script inside a checkout of android/external/openssl to generate a json file
 # containing the android tag to openssl version mapping
 
-output=`dirname $(readlink -f $0)`'/input/tag_to_openssl_version.json'
+project_id=$1
+project_key=`echo $project_id | tr a-z A-Z`"_VERSION"
+
+output=`dirname $(readlink -f $0)`"/input/tag_to_${project_id}_version.json"
 echo '{' > $output 
 for tag in `git tag`
 do
 	git checkout --quiet $tag
-	if [ -f openssl.version ]
+	if [ -f "${project_id}.version" ]
 	then
-		version=`cat openssl.version | cut -d'=' -f 2 | grep -v '#'`
+		version=`cat "${project_id}.version" | grep $project_key | cut -d'=' -f 2 | grep -v '#'`
 		echo "    \"$tag\" : \"$version\"," >> $output
 	fi
 done
