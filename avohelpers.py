b74a4de4c67fa8e7dc3985f676fff09447a1ed6f
Daniel Thomas <drt24@cam.ac.uk>
2015-10-02 10:31:05 +0100

    Make paths to images absolute as pngs appear to be treated as absolute by default while svgs are relative and that causes confusion

--- avohelpers.py
+++ avohelpers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
 def insert_svg(filebase, alt, width, height):
-	print('''<object type="image/svg+xml" data="{filebase}.svg" alt="{alt}">
-  <img src="{filebase}.png" width="{width}" height="{height}" alt="{alt}"/>
+	print('''<object type="image/svg+xml" data="/{filebase}.svg" alt="{alt}">
+  <img src="/{filebase}.png" width="{width}" height="{height}" alt="{alt}"/>
 </object>'''.format(filebase=filebase, alt=alt, width=width, height=height))
