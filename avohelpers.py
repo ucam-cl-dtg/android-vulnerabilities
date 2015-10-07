f4fc56bbb0cf2ae0efdabf9ccfa914c53483cbb0
Daniel Thomas <drt24@cam.ac.uk>
2015-10-07 18:18:44 +0100

    Support inserted svgs being links (a bit anyway)

--- avohelpers.py
+++ avohelpers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-def insert_svg(filebase, alt, width, height):
-	print('''<object type="image/svg+xml" data="/{filebase}.svg" alt="{alt}">
+def insert_svg(filebase, alt, width, height, link=False):
+	style = 'pointer-events: none;' if link else ''
+	print('''<object type="image/svg+xml" data="/{filebase}.svg" alt="{alt}" style="{style}">
   <img src="/{filebase}.png" width="{width}" height="{height}" alt="{alt}"/>
-</object>'''.format(filebase=filebase, alt=alt, width=width, height=height))
+</object>'''.format(filebase=filebase, alt=alt, width=width, height=height, style=style))
