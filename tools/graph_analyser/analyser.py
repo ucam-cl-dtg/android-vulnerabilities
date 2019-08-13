71b4a27f227ae8d46044066adc7a17260a8f2a31
Daniel Carter <dcc52@cam.ac.uk>
2019-08-13 16:18:32 +0100

    Reduced duplication of code between graph analyser tools

--- tools/graph_analyser/analyser.py
+++ tools/graph_analyser/analyser.py
@@ -1,54 +1,16 @@
 # Copyright (C) Daniel Carter 2019
 # Licenced under the 2-clause BSD licence
 
-import pygraphviz as pgv
+from graph_utils import *
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors, patches
-import datetime
-import os
-import json
 from collections import OrderedDict
 
 START_YEAR = 2009
 
-def strictify(graph, directed=True):
-    result = pgv.AGraph(strict=True, directed=directed)
-    result.add_edges_from(graph.edges())
-    return result
-
-def dfs(graph, start, end):
-    visited = set()
-    to_visit = [start]
-    while len(to_visit) != 0:
-        item = to_visit.pop()
-        if item == end:
-            return True
-        visited.add(item)
-        for neighbor in graph.out_neighbors(item):
-            if neighbor not in visited:
-                to_visit.append(neighbor)
-    return False
-
-def print_search(graph, start, end):
-    print('{start} -> {end}: {result}'.format(start=start, end=end, result=dfs(graph, start, end)))
-
-def dates_to_today(start_year):
-    today = datetime.date.today()
-    dates = []
-    for year in range(start_year, today.year + 1):
-        for month in range(1, 13):
-            date = datetime.date(year, month, 1)
-            if date > today:
-                return dates
-            dates.append(date)
-
-path = '../../input/release_dates.json'
-versions = []
-if os.path.isfile(path):
-    with open(path, 'r') as f:
-        rjson = json.load(f)
-        versions = list(rjson.keys())
+versions = load_version_list('../../input/release_dates.json')
 
 #versions.append('all')
 dates = dates_to_today(START_YEAR)
@@ -60,56 +22,37 @@ for vindex, version in enumerate(versions):
 
     for dindex, date in enumerate(dates):
         path = '../../output/graphs/{:s}/graph-{:d}-{:02d}.gv'.format(version, date.year, date.month)
-        if not os.path.isfile(path):
+        graph = import_graph(path)
+        if graph is None:
             grid[vindex, dindex] = -1
             continue
 
-        # Import graph
-        graph = pgv.AGraph(path)
-
-        # Remove red (device-specific) edges
-        graph.delete_edges_from([edge for edge in graph.edges() if edge.attr['color'] == 'red'])
-
         # Experimental: remove fixed edges
-        #graph.delete_edges_from([edge for edge in graph.edges() if edge.attr['style'] == 'dashed'])
+        #graph = remove_patched(graph)
 
-        # Add 'backwards' edges
-        hierarchy = ['remote', 'proximity', 'network', 'user', 'access-to-data', 'modify-apps', 'control-hardware', 'system', 'unlock-bootloader', 'tee', 'root', 'kernel']
-        for start in hierarchy:
-            for end in hierarchy:
-                if start == end:
-                    break
-                graph.add_edge(start, end)
-
-        # Remove duplicate edges
+        # Add "backwards" edges and remove duplicates
+        add_backwards_edges(graph)
         sgraph = strictify(graph)
 
         # Sequence of odd numbered 'priorities' as they are unambiguously between even numbered limits below
         if dfs(sgraph, 'remote', 'kernel'):
             grid[vindex, dindex] = 17
-            continue
-        if dfs(sgraph, 'remote', 'system'):
+        elif dfs(sgraph, 'remote', 'system'):
             grid[vindex, dindex] = 15
-            continue
-        if dfs(sgraph, 'remote', 'user'):
+        elif dfs(sgraph, 'remote', 'user'):
             grid[vindex, dindex] = 13
-            continue
-        if dfs(sgraph, 'network', 'kernel'):
+        elif dfs(sgraph, 'network', 'kernel'):
             grid[vindex, dindex] = 11
-            continue
-        if dfs(sgraph, 'network', 'system'):
+        elif dfs(sgraph, 'network', 'system'):
             grid[vindex, dindex] = 9
-            continue
-        if dfs(sgraph, 'network', 'user'):
+        elif dfs(sgraph, 'network', 'user'):
             grid[vindex, dindex] = 7
-            continue
-        if dfs(sgraph, 'user', 'kernel'):
+        elif dfs(sgraph, 'user', 'kernel'):
             grid[vindex, dindex] = 5
-            continue
-        if dfs(sgraph, 'user', 'system'):
+        elif dfs(sgraph, 'user', 'system'):
             grid[vindex, dindex] = 3
-            continue
-        grid[vindex, dindex] = 1
+        else:
+            grid[vindex, dindex] = 1
 
 # Store colours
 colours = OrderedDict()
--- tools/graph_analyser/scorer.py
+++ tools/graph_analyser/scorer.py
@@ -1,71 +1,16 @@
 # Copyright (C) Daniel Carter 2019
 # Licenced under the 2-clause BSD licence
 
-import pygraphviz as pgv
+from graph_utils import *
+
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import colors, patches
-import datetime
-import os
-import json
 import math
-import re
-from collections import OrderedDict
 
 START_YEAR = 2009
 
-def strictify(graph, directed=True):
-    result = pgv.AGraph(strict=True, directed=directed)
-    result.add_edges_from(graph.edges())
-    return result
+versions = load_version_list('../../input/release_dates.json')
 
-def dfs(graph, start, end):
-    visited = set()
-    to_visit = [start]
-    while len(to_visit) != 0:
-        item = to_visit.pop()
-        if item == end:
-            return True
-        visited.add(item)
-        for neighbor in graph.out_neighbors(item):
-            if neighbor not in visited:
-                to_visit.append(neighbor)
-    return False
-
-def print_search(graph, start, end):
-    print('{start} -> {end}: {result}'.format(start=start, end=end, result=dfs(graph, start, end)))
-
-def dates_to_today(start_year):
-    today = datetime.date.today()
-    dates = []
-    for year in range(start_year, today.year + 1):
-        for month in range(1, 13):
-            date = datetime.date(year, month, 1)
-            if date > today:
-                return dates
-            dates.append(date)
-
-def count_edge(edge):
-    '''Count the number of vulnerabilities represented by a given edge'''
-    text = edge.attr['label']
-    if text == '':
-        return 0
-    # Look for a number on the label
-    number = re.search(r'(?<=\()[0-9]*(?=\ vulnerabilities\))', text)
-    if number == None:
-        # If there isn't one, it's only for one vulnerability
-        return 1
-    else:
-        return int(number.group())
-
-path = '../../input/release_dates.json'
-versions = []
-if os.path.isfile(path):
-    with open(path, 'r') as f:
-        rjson = json.load(f)
-        versions = list(rjson.keys())
-
-#versions.append('all')
 dates = dates_to_today(START_YEAR)
 
 #prev = ['','','']
@@ -88,31 +33,19 @@ for version in versions:
 
     for dindex, date in enumerate(dates):
         path = '../../output/graphs/{:s}/graph-{:d}-{:02d}.gv'.format(version, date.year, date.month)
-        if not os.path.isfile(path):
+        graph = import_graph(path)
+        if graph is None:
             line[dindex] = -1
             continue
 
-        # Import graph
-        graph = pgv.AGraph(path)
-
-        # Remove red (device-specific) edges
-        graph.delete_edges_from([edge for edge in graph.edges() if edge.attr['color'] == 'red'])
-
-        # Make a copy and remove patched edges
-        unpatched = graph.copy()
-        unpatched.delete_edges_from([edge for edge in unpatched.edges() if edge.attr['style'] == 'dashed'])
+        # Remove patched edges
+        unpatched = remove_patched(graph)
 
         # Count the number of vulnerabilities, and double-weight unpatched ones (which are in both lists)
         score = sum([count_edge(edge) for edge in graph.edges()]) + sum([count_edge(edge) for edge in unpatched.edges()])
         line[dindex] = 2.0 / (1.0 + math.exp(0.01 * score))
 
-        # Add 'backwards' edges
-        hierarchy = ['remote', 'proximity', 'network', 'user', 'access-to-data', 'modify-apps', 'control-hardware', 'system', 'unlock-bootloader', 'tee', 'root', 'kernel']
-        for start in hierarchy:
-            for end in hierarchy:
-                if start == end:
-                    break
-                graph.add_edge(start, end)
+        add_backwards_edges(graph)
 
         # Remove duplicate edges
         sgraph = strictify(graph)
@@ -120,33 +53,26 @@ for version in versions:
         # Add a value to the score based on the level of possible exploitation (by chaining exploits or otherwise)
         if dfs(sgraph, 'remote', 'kernel'):
             line[dindex] += 0.0
-            continue
-        if dfs(sgraph, 'remote', 'system'):
+        elif dfs(sgraph, 'remote', 'system'):
             line[dindex] += 0.1
-            continue
-        if dfs(sgraph, 'remote', 'user'):
+        elif dfs(sgraph, 'remote', 'user'):
             line[dindex] += 0.2
-            continue
-        if dfs(sgraph, 'network', 'kernel'):
+        elif dfs(sgraph, 'network', 'kernel'):
             line[dindex] += 0.3
-            continue
-        if dfs(sgraph, 'network', 'system'):
+        elif dfs(sgraph, 'network', 'system'):
             line[dindex] += 0.4
-            continue
-        if dfs(sgraph, 'network', 'user'):
+        elif dfs(sgraph, 'network', 'user'):
             line[dindex] += 0.5
-            continue
-        if dfs(sgraph, 'user', 'kernel'):
+        elif dfs(sgraph, 'user', 'kernel'):
             line[dindex] += 0.6
-            continue
-        if dfs(sgraph, 'user', 'system'):
+        elif dfs(sgraph, 'user', 'system'):
             line[dindex] += 0.7
-            continue
-        line[dindex] += 1
+        else:
+            line[dindex] += 1
 
-    indices = line>0
     plt.plot(np.array(dates)[line>0], line[line>0], label=version)
 
+plt.ylim([0,2])
 plt.legend()
 
 plt.show()
